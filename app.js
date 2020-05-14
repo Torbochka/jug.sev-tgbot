c3a63476bf9aaf702250252800bfc6b01ae54caa
torbochka <sasha.torbek@gmail.com>
2020-05-15 01:35:12 +0300

    Обработал команды, добавил заглушку слушателю, добавил зависимости
    - Команда /start, слушатель "Встречи"
    - Зависимости: axios, prismic-javascript, nodemon

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 require('dotenv').config();
+const axios = require('./axios');
 const { Telegraf } = require('telegraf');
 
+const Markup = require('telegraf/markup');
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome!'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.use(Telegraf.log());
+
+bot.start(({ reply }) => {
+  return reply(
+    '🤟Встречи',
+    Markup.keyboard(['🤟Встречи']).oneTime().resize().extra()
+  );
+});
+
+bot.hears('🤟Встречи', ({ reply }) => {
+  return reply(
+    'Предыдущие встречи',
+    Markup.keyboard([
+      ['Встреча 1', 'Встреча 2'],
+      ['Встреча 3', '📞Встреча 4']
+    ])
+      .oneTime()
+      .resize()
+      .extra()
+  );
+});
+
 bot.launch();
+
+const getMeetups = () => {
+  return [];
+};
